f2c2e17c23d062dec914f21a8baea0160f244894
ladutia <alex.ladutia@gmail.com>
2014-05-19 18:45:53 +0300

    two

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2014-05-16 07:18:33 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2014-05-16	07:18:33	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
